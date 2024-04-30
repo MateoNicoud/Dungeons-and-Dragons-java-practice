93b438d02409f5e3752f219afcbaa49257566545
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-04-30 08:24:46 +0200

    added inventory

---
+++ src/Stuff/Items.java
@@ -0,0 +1,33 @@
+package Stuff;
+
+public abstract class Items {
+    private String name;
+    private String type;
+
+    public Items(String type, String name) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
